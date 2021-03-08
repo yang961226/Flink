5ab5dd1d0d6ccbef85b9c00936ad7607c457be43
SundayTing <524250484@qq.com>
2021-03-08 13:41:24 +0800

    *项目建设 1.初步实现设置页，底部导航条未实现，功能未实现

--- app/src/main/java/com/example/flink/SettingsActivity.java
+++ app/src/main/java/com/example/flink/SettingsActivity.java
@@ -1,17 +1,22 @@
 package com.example.flink;
 
 import android.os.Message;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.flink.layout.note.SettingLine;
+import com.example.flink.tools.notify.ToastUtil;
+
+import butterknife.BindView;
 
 public class SettingsActivity extends FlinkBaseActivity {
+
+    @BindView(R.id.ll_content)
+    LinearLayout llContent;
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_settings;
-    }
-
-    @Override
-    public void flinkMessageCallBack(Message msg) {
-
     }
 
     @Override
@@ -21,8 +26,37 @@ public class SettingsActivity extends FlinkBaseActivity {
 
     @Override
     protected void initView() {
-
+        addSwitchSettingLine("12/24小时制");
+        addSwitchSettingLine("触觉反馈");
+        addNormalSettingLine("评价这款APP", v -> {
+            ToastUtil.show(this, "点击了评价");
+        });
+        addNormalSettingLine("联系开发者", v -> {
+            ToastUtil.show(this, "点击了联系开发者");
+        });
     }
 
+    /**
+     * 构建一个切换的设置行
+     *
+     * @param title 标题
+     */
+    private void addSwitchSettingLine(String title) {
+        SettingLine settingLine = new SettingLine(this);
+        settingLine.setTitle(title);
+        llContent.addView(settingLine);
+    }
+
+    private void addNormalSettingLine(String title, View.OnClickListener onClickListener) {
+        SettingLine settingLine = new SettingLine(this);
+        settingLine.setTitle(title);
+        settingLine.makeSwitchToButton(onClickListener);
+        llContent.addView(settingLine);
+    }
+
+    @Override
+    public void flinkMessageCallBack(Message msg) {
+
+    }
 
 }
---
+++ app/src/main/java/com/example/flink/layout/note/SettingLine.java
@@ -0,0 +1,100 @@
+package com.example.flink.layout.note;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.example.flink.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * 设置页中的一行，右边默认是点击按钮，也可以自己设置自定义布局
+ */
+public class SettingLine extends ConstraintLayout {
+
+    @BindView(R.id.ll_clickable_content)
+    LinearLayout llClickableContent;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.ibtn_switch)
+    ImageButton ibtnSwitch;
+
+    private boolean switchToButton;//开关是否退化成单纯的按钮
+    private OnClickListener onClickListener;//切换成按钮之后用到的回调
+
+    private boolean switchState;//切换状态，如果是自定义布局的状态下，这个属性没用
+
+    public SettingLine(@NonNull Context context) {
+        super(context);
+        init();
+    }
+
+    public SettingLine(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SettingLine(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_setting_line, this);
+        ButterKnife.bind(this, view);
+    }
+
+    /**
+     * 将开关退化成单纯的按钮
+     */
+    public void makeSwitchToButton(OnClickListener onClickListener) {
+        switchToButton = true;
+        this.onClickListener = onClickListener;
+        ibtnSwitch.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_button_hand, null));
+    }
+
+    /**
+     * 如果点击的是单纯的按钮
+     */
+    protected void clickButton() {
+        onClickListener.onClick(ibtnSwitch);
+    }
+
+    /**
+     * 如果点击的是开关
+     */
+    protected void clickSwitch() {
+        ibtnSwitch.setBackgroundResource(switchState ? R.drawable.ic_switch_close : R.drawable.ic_switch_open);
+        switchState = !switchState;
+    }
+
+    public String getTitle() {
+        return tvTitle.getText().toString();
+    }
+
+    public void setTitle(String title) {
+        tvTitle.setText(title);
+    }
+
+    @OnClick(R.id.ibtn_switch)
+    public void onViewClicked() {
+        //如果已经是按钮状态的话，就不响应swtich事件了，而是响应点击事件
+        if (switchToButton) {
+            clickButton();
+            return;
+        }
+        clickSwitch();
+    }
+}
